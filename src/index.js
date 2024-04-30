9bfdd6558fd57ee885d4632023c0aa14fd6185aa
JeyCarlitos <perezflorianjuancarlos21@gmail.com>
2024-04-29 22:25:15 -0400

    cambios en index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Definición de funciones o variables
+function saludar(nombre) {
+    console.log("¡Hola, " + nombre + "!");
+}
+
+var numero = 42;
+
+// Llamada a la función y uso de la variable
+saludar("Mundo");
+console.log("El número es: " + numero);
